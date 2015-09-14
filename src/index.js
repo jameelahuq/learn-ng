5bc64bcd802c9b170e3aaaeac52e4c9ef0042e76
Samer Buna <samer.buna@gmail.com>
2015-09-14 16:34:22 -0700

    Implement tweet length logic

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
-let hot = "HOT";
-console.log(`ES2015 is ${hot}`);
+import angular from "angular";
+
+let app = angular.module("twitterClone", []);
+
+app.controller("TweetCtrl", function() {
+  this.tweet = '';
+
+  this.invalidTweetLength = function() {
+    return this.tweet.length === 0 || this.remainingCharacters() < 0;
+  };
+
+  this.remainingCharacters = function() {
+    return 140 - this.tweet.length;
+  }
+});
