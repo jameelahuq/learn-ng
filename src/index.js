976b3fb596be23d243e124e8abc35cae4f8368f7
Samer Buna <samer.buna@gmail.com>
2015-09-14 18:02:10 -0700

    Perfect loop

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import angular from "angular";
+import _ from "lodash";
 
 let app = angular.module("twitterClone", []);
 
-app.controller("TweetCtrl", function($scope) {
+app.controller("TweetCtrl", function($scope, $http) {
   $scope.tweet = '';
+
+  $http.get("https://www.reddit.com/r/perfectloops.json")
+      .success( data => {
+        $scope.allPerfectLoops = data.data.children
+          .map(function(rp) { return rp.data.url } )
+          .filter(function(imgUrl) { return imgUrl.match(/gif$/) })
+  });
 
   $scope.invalidTweetLength = () => $scope.tweet.length === 0 || $scope.remainingCharacters() < 0;
   $scope.remainingCharacters = () => 140 - $scope.tweet.length;
@@ -11,6 +19,11 @@ app.controller("TweetCtrl", function($scope) {
   $scope.tweetList = [];
   $scope.sendTweet = () => {
     $scope.tweetList.unshift($scope.tweet);
+
+    if ($scope.tweet.match(/(?:\b|^)#perfectloop(?:\b|$)/)) {
+      $scope.perfectLoop = _.sample($scope.allPerfectLoops);
+    }
+
     $scope.tweet = '';
   };
 });
